59b72277f7b4860ef83a1259a41160932f444459
Nikita Gorlin <madnix7997@gmail.com>
2017-05-21 01:08:35 +0700

    xor, logging

--- client/client.py
+++ client/client.py
@@ -1,96 +1,154 @@
 import struct
 import socket
+import logging
+import sys
+import os
+from Crypto.Cipher import XOR
+
+MAX_ITER = 20
 
 
-def checkSum(infString):
-    sum = 0
-    countTo = (len(infString) // 2) * 2
+def parse_input():
+    try:
+        file_name = sys.argv[sys.argv.index('-f') + 1]
+        address = sys.argv[sys.argv.index('-adr') + 1]
+        crypt = '-cr' in sys.argv
+        return file_name, address, crypt
+    except ValueError:
+        print('Format: {} [-f <file_name>] [-adr <address>] [-cr]'.format(sys.argv[0]))
+        exit(0)
+    except IndexError:
+        print('Format: {} [-f <file_name>] [-adr <address>] [-cr]'.format(sys.argv[0]))
+        exit(0)
+
+
+def set_logger():
+    logging.basicConfig(format='%(filename)s[LINE:%(lineno)d]# %(levelname)-8s [%(asctime)s] %(message)s',
+                        level=logging.DEBUG,
+                        filename='client.log')
+
+
+def check_sum(inf_string):
+    hash_sum = 0
+    count_to = (len(inf_string) // 2) * 2
     count = 0
 
-    while count < countTo:
-        thisVal = ord(infString[count + 1]) * 256 + ord(infString[count])
-        sum += thisVal
-        sum &= 0xffffffff
+    while count < count_to:
+        this_val = ord(inf_string[count + 1]) * 256 + ord(inf_string[count])
+        hash_sum += this_val
+        hash_sum &= 0xffffffff
         count += 2
-    if countTo < len(infString):
-        sum += ord(infString[countTo])
-        sum &= 0xffffffff
-    sum = (sum >> 16) + sum & 0xffff
-    sum += sum >> 16
-    answer = ~sum
+    if count_to < len(inf_string):
+        hash_sum += ord(inf_string[count_to])
+        hash_sum &= 0xffffffff
+    hash_sum = (hash_sum >> 16) + hash_sum & 0xffff
+    hash_sum += hash_sum >> 16
+    answer = ~hash_sum
     answer &= 0xffff
     answer = answer >> 8 | (answer << 8 & 0xffff)
     return answer
 
 
-def createPacket(id, num, message):
+def create_packet(id, num, message):
     header = struct.pack('bbHHh', 8, 0, 0, id, num)
     packet = header + message
-    check = checkSum(str(packet))
+    check = check_sum(str(packet))
     header = struct.pack('bbHHh', 8, 0, check, id, num)
     return header + message
 
 
-def sendFile(filename, addr):
+def send_packet(sock, packet, expected_response, addr):
+    code = 0
+    i = 0
+    while True:
+        try:
+            if i == MAX_ITER:
+                return False
+            if code == 0:
+                sock.sendto(packet, (addr, 1))
+                code = 1
+            data, address = sock.recvfrom(1508)
+            if struct.unpack('b', data[20:21])[0] != 0:
+                continue
+            answer = (data[28:len(data)]).decode()
+            if answer == expected_response or answer == 'again':
+                return True
+            code = 0
+        except socket.timeout:
+            if len(packet) == 8:
+                print('lol')
+            code = 0
+            i += 1
+            continue
+
+
+def send_file(file_name, addr, crypt):
     s = socket.socket(socket.AF_INET, socket.SOCK_RAW, socket.IPPROTO_ICMP)
-    s.settimeout(5)
+    s.settimeout(0.5)
 
     id = 4
     num = 0
-    packet = createPacket(id, num, filename.encode())
+    message = file_name
+    cipher = XOR.new((open('key.txt', 'r')).readline())
+    if crypt:
+        message += ' true'
+    else:
+        message += ' false'
 
-    code = 0
-    while True:
-        try:
-            if code == 0:
-                s.sendto(packet, (addr, 1))
-                code = 1
-            data, address = s.recvfrom(1508)
-            if struct.unpack('b', data[20:21])[0] != 0:
-                continue
-            if (data[28:len(data)]).decode() == 'opened':
-                break
-            if (data[28:len(data)]).decode() == 'corrupted':
-                code = 0
-                continue
-        except socket.timeout:
-            code = 0
-            continue
+    packet = create_packet(id, num, message.encode())
 
-    f = open(filename, 'rb')
+    logging.info('Sending filename to server')
+
+    if send_packet(s, packet, 'opened', addr):
+        logging.info('Filename was sent to server')
+    else:
+        logging.error('Server is not available')
+        return
+    num += 1
+
+    logging.info('Sending of file started')
+
+    file_size = os.path.getsize(file_name)
+    f = open(file_name, 'rb')
     message = f.read(1472)
     all_len = 0
+
+    progress = 0
+    num_data_sent = 0
     while message:
+        all_len += len(message)
+        logging.debug('Sending packet {}'.format(num))
+        if crypt:
+            message = cipher.encrypt(message)
+        packet = create_packet(id, num, message)
+
+        if send_packet(s, packet, 'correct', addr):
+            logging.debug('Packet {} was sent successful'.format(num))
+        else:
+            logging.error('Server is not available')
+            return
+        num_data_sent += len(message)
+        message = f.read(1472)
+
+        if int((num_data_sent / file_size) * 100) > progress:
+            progress = int(num_data_sent / file_size * 100)
+            print(str(progress) + '%')
+
         if num + 1 < 0xffff:
             num += 1
         else:
             num = 0
-        all_len += len(message)
-        packet = createPacket(id, num, message)
-        code = 0
-        while True:
-            try:
-                if code == 0:
-                    s.sendto(packet, (addr, 1))
-                    code = 1
 
-                data, address = s.recvfrom(1508)
-                if struct.unpack('b', data[20:21])[0] != 0:
-                    continue
-                if (data[28:len(data)]).decode() == 'correct':
-                    break
-                if (data[28:len(data)]).decode() == 'corrupted':
-                    code = 0
-                    continue
-            except socket.timeout:
-                code = 0
-                continue
-        message = f.read(1472)
-        print(num)
-    packet = createPacket(id, num, b'')
-    s.sendto(packet, (addr, 1))
+    logging.debug('Sending last packet')
+
+    packet = create_packet(id, num, b'')
+    if send_packet(s, packet, '', addr):
+        logging.info('File was sent to server successfully')
+    else:
+        logging.error('Server is not available')
+        return
 
 
-filename = input()
-addr = input()
-sendFile(filename, addr)
+set_logger()
+filename, address, crypt_status = parse_input()
+send_file(filename, address, crypt_status)
--- server/server.py
+++ server/server.py
@@ -1,70 +1,122 @@
 import socket
 import struct
+import logging
+import datetime
+import os
+from Crypto.Cipher import XOR
 
 
-def checkSum(infString):
-    sum = 0
-    countTo = (len(infString) // 2) * 2
+def set_logger():
+    logging.basicConfig(format='%(filename)s[LINE:%(lineno)d]# %(levelname)-8s [%(asctime)s] %(message)s',
+                        level=logging.DEBUG,
+                        filename='server.log')
+
+
+def check_sum(inf_string):
+    hash_sum = 0
+    count_to = (len(inf_string) // 2) * 2
     count = 0
 
-    while count < countTo:
-        thisVal = ord(infString[count + 1]) * 256 + ord(infString[count])
-        sum += thisVal
-        sum &= 0xffffffff
+    while count < count_to:
+        this_val = ord(inf_string[count + 1]) * 256 + ord(inf_string[count])
+        hash_sum += this_val
+        hash_sum &= 0xffffffff
         count += 2
-    if countTo < len(infString):
-        sum += ord(infString[countTo])
-        sum &= 0xffffffff
-    sum = (sum >> 16) + sum & 0xffff
-    sum += sum >> 16
-    answer = ~sum
+    if count_to < len(inf_string):
+        hash_sum += ord(inf_string[count_to])
+        hash_sum &= 0xffffffff
+    hash_sum = (hash_sum >> 16) + hash_sum & 0xffff
+    hash_sum += hash_sum >> 16
+    answer = ~hash_sum
     answer &= 0xffff
     answer = answer >> 8 | (answer << 8 & 0xffff)
     return answer
 
 
-def checkCorrect(packet):
-    type, code, expectedSum, id, num = struct.unpack('bbHHh', packet[20:28])
+def check_correct(packet):
+    type, code, expected_sum, id, num = struct.unpack('bbHHh', packet[20:28])
     message = packet[28:len(packet)]
     header = struct.pack('bbHHh', type, code, 0, id, num)
-    realSum = checkSum(str(header + message))
-    return realSum == expectedSum
+    real_sum = check_sum(str(header + message))
+    return real_sum == expected_sum
 
 
-def createPacket(id, num, message):
+def create_packet(id, num, message):
     header = struct.pack('bbHHh', 0, 0, 0, id, num)
     packet = header + message
-    check = checkSum(str(packet))
+    check = check_sum(str(packet))
     header = struct.pack('bbHHh', 0, 0, check, id, num)
     return header + message
+
+
+def give_unique_name(file_name):
+    if os.path.exists(file_name):
+        i = 1
+        while True:
+            if os.path.exists(str(i) + '_' + file_name):
+                i += 1
+            else:
+                break
+        return str(i) + '_' + file_name
+    else:
+        return file_name
 
 
 def listen():
     s = socket.socket(socket.AF_INET, socket.SOCK_RAW, socket.IPPROTO_ICMP)
     s.bind(('', 14900))
+    info = open('work_history.txt', 'a')
+    cipher = XOR.new((open('key.txt', 'r')).readline())
     places = {}
-    num = 1
-    while 1:
+    while True:
         data, address = s.recvfrom(1508)
-        packetNum = struct.unpack('H', data[26:28])[0]
-        if not checkCorrect(data):
-            print('Data corrupted')
-            s.sendto(createPacket(0, packetNum, 'corrupted'.encode()), address)
+        if struct.unpack('b', data[20:21])[0] != 8:
             continue
 
-        if len(data) == 28:
-            item = places.pop(address[0])
-            print(item[1])
-            item[0].close()
-            break
-        elif address[0] not in places.keys():
-            places[address[0]] = [open((data[28:len(data)]).decode(), 'wb'), 0]
-            s.sendto(createPacket(0, packetNum, 'opened'.encode()), address)
-        elif packetNum == places[address[0]][1] + 1:
-            print('Correct')
-            print(packetNum)
-            places[address[0]][1] += 1
-            places[address[0]][0].write(data[28:len(data)])
-            s.sendto(createPacket(0, packetNum, 'correct'.encode()), address)
+        logging.info('Received packet from {} '.format(address[0]))
 
+        packet_num = struct.unpack('H', data[26:28])[0]
+
+        if not check_correct(data):
+            logging.debug('Corrupted data in packet')
+            print('Data corrupted')
+            s.sendto(create_packet(0, packet_num, 'corrupted'.encode()), address)
+            continue
+
+        if address[0] not in places.keys():
+            if len(data) == 28:
+                s.sendto(create_packet(0, packet_num, b''), address)
+                continue
+
+            file_name, crypt = (data[28:len(data)]).decode().split(' ')
+            uniq_name = give_unique_name(file_name)
+            about_file = '\nFile: {}\nSaved as: {}\nReceived from: {}\nTime: {}\n'.format(
+                file_name, uniq_name, address[0], datetime.datetime.now()
+            )
+            logging.info('New client. Opening new file {} for acceptance'.format(uniq_name))
+            places[address[0]] = [open(uniq_name, 'wb'), 0, about_file, crypt]
+            s.sendto(create_packet(0, packet_num, 'opened'.encode()), address)
+
+        elif packet_num == places[address[0]][1] + 1 or places[address[0]][1] + 1 == 0xffff and packet_num == 0:
+            if len(data) == 28:
+                logging.info('Received end of file. Closing file')
+                s.sendto(create_packet(0, packet_num, b''), address)
+                item = places.pop(address[0])
+                item[0].close()
+                info.write(item[2])
+            else:
+                logging.debug('Correct data in packet')
+                places[address[0]][1] = packet_num
+                message = data[28:len(data)]
+                if places[address[0]][3] == 'true':
+                    message = cipher.decrypt(message)
+                places[address[0]][0].write(message)
+                s.sendto(create_packet(0, packet_num, 'correct'.encode()), address)
+
+        elif packet_num == places[address[0]][1]:
+            logging.debug('again')
+            s.sendto(create_packet(0, packet_num, 'again'.encode()), address)
+
+
+set_logger()
 listen()
